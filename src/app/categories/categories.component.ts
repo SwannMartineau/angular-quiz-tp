e02c1cb823e646a48deea87a74c7bdc3e5f0d1e1
nicolas.bidet@efrei.net <nicolas.bidet@efrei.net>
2024-10-16 11:28:28 +0200

    first step of component

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../shared/services/categories.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categoriesContent: any[] = this.categoriesService.categoriesContent;
 
+  constructor(private categoriesService: CategoriesService) { }
+
+  ngOnInit(): void {
+    this.categoriesService.getCategories();
+  }
 }
